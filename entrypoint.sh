1051346af0e99770b9078de550adb04a4013eed0
Tianhao Zhou <tianhaoz@umich.edu>
2020-09-16 00:13:17 -0700

    fix: add file exist checking

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,23 @@ set -o pipefail
 ORIGINAL_FILE="$GITHUB_WORKSPACE/$1"
 MIRROR_FILE="$GITHUB_WORKSPACE/$2"
 
+if [ -f "$ORIGINAL_FILE" ]; then
+  echo "INFO: $ORIGINAL_FILE exists."
+else 
+  echo "ERROR: $ORIGINAL_FILE does not exist. Abort."
+  exit 1
+fi
+
+if [ -f "$MIRROR_FILE" ]; then
+  echo "INFO: $MIRROR_FILE exists."
+else 
+  echo "ERROR: $MIRROR_FILE does not exist. Abort."
+  exit 1
+fi
+
 if cmp -s "$ORIGINAL_FILE" "$MIRROR_FILE"
 then
-  echo "Content in $ORIGINAL_FILE and $MIRROR_FILE are the same. Done."
+  echo "INFO: content in $ORIGINAL_FILE and $MIRROR_FILE are the same. Done."
 else
   echo "ERROR: content in $ORIGINAL_FILE and $MIRROR_FILE are different."
   exit 1
